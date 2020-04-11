d1c0ef33cb4b8c36441c560026624f03a390b717
TarasLesh <t.leshchyshyn@gmail.com>
2020-04-11 23:45:16 +0300

    first lesson ng-owu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserModel} from '../models/UserModel';
+import {UserService} from './services/user.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello {{msg}}</h1>
+  <app-user *ngFor="let u of users" [user]="u"></app-user>`,
+  styles: [`h1 {
+    background: aqua
+  }`]
 })
 export class AppComponent {
   title = 'ng-owu';
+  msg = 'users';
+  users: UserModel[];
+
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe(value => this.users = value);
+  }
 }
