c25600ad539d137ce27c7b6ab2f202efd54493f2
TarasLesh <t.leshchyshyn@gmail.com>
2020-05-11 19:25:12 +0300

    ng forms lecture OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,58 @@
 import {Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {UserModel} from '../models/UserModel';
-import {UserService} from './services/user.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello {{msg}}</h1>
-  <app-user *ngFor="let u of users" [user]="u"></app-user>`,
+  templateUrl: 'app.component.html',
   styles: [`h1 {
     background: aqua
   }`]
 })
 export class AppComponent {
-  title = 'ng-owu';
-  msg = 'users';
-  users: UserModel[];
 
-  constructor(private userService: UserService) {
-    this.userService.getUsers().subscribe(value => this.users = value);
+  // state: { login: string, pass: string } = {
+  //   login: '',
+  //   pass: ''
+  // };
+
+  // loginForm: { login: string, password: string } = {
+  //   login: 'def log',
+  //   password: 'def pass'
+  // };
+
+  forma: FormGroup;
+
+  // password: FormControl = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]{6,}')]);
+  //
+  // constructor() {
+  //   this.forma = new FormGroup({
+  //     login: new FormControl('', [Validators.required, Validators.maxLength(10)
+  //       // ,(control) => {
+  //       //   if (control.value.length < 5) {
+  //       //     return {customError: true};
+  //       //   }
+  //       // }
+  //       ] ),
+  //     password: this.password
+  //   });
+  // }
+  //
+  //
+  // // changeState(xxx: HTMLInputElement) {
+  //
+  // //   this.state = xxx.value;
+  // // }
+  // checkState(forma: FormGroup) {
+  //   console.log(forma);
+  // }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.forma = formBuilder.group({
+      login: ['', [Validators.required, Validators.maxLength(10)]],
+      password: ['', [Validators.required, Validators.pattern('[a-zA-Z]{6,}')]]
+    });
   }
+
+  // https://regex101.com/   generaror regulars
+
 }
